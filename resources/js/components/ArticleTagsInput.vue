7af833768ebcdff2d03026ce21c01f57f22c2b18
kasyu-create <63037931+kasyu-create@users.noreply.github.com>
2020-09-24 20:57:51 +0900

    タグの自動補完を行う タグをハッシュタグ風に表示する

--- resources/js/components/ArticleTagsInput.vue
+++ resources/js/components/ArticleTagsInput.vue
@@ -21,22 +21,15 @@ export default {
       type: Array,
       default: [],
     },
+        autocompleteItems: {
+      type: Array,
+      default: [],
+    },
   },
   data() {
     return {
       tag: '',
       tags: this.initialTags, 
-      autocompleteItems: [{
-        text: 'Spain',
-      }, {
-        text: 'France',
-      }, {
-        text: 'USA',
-      }, {
-        text: 'Germany',
-      }, {
-        text: 'China',
-      }],
     };
   },
   computed: {
@@ -64,5 +57,8 @@ export default {
     margin-right: 4px;
     border-radius: 0px;
     font-size: 13px;
+  }
+  .vue-tags-input .ti-tag::before {
+    content: "#";
   }
 </style>
