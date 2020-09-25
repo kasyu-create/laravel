6c89003999f74bd596c777f3c2c2d7545103c964
kasyu-create <63037931+kasyu-create@users.noreply.github.com>
2020-09-25 11:21:56 +0900

    フォローボタンのVueコンポーネントからLaravelに非同期通信する フォロー中・フォロワーの数を表示する ユーザーページに記事一覧を表示する いいねした記事一覧を表示する

--- resources/js/components/FollowButton.vue
+++ resources/js/components/FollowButton.vue
@@ -1,9 +1,7 @@
 <template>
   <div>
     <button
-      class="btn-sm shadow-none border border-primary p-2"
-      :class="buttonColor"
-    >
+      class="btn-sm shadow-none border border-primary p-2" :class="buttonColor" @click="clickFollow">
       <i
         class="mr-1"
         :class="buttonIcon"
@@ -19,6 +17,13 @@
       initialIsFollowedBy: {
         type: Boolean,
         default: false,
+      },
+      authorized: {
+        type: Boolean,
+        default: false,
+      },
+      endpoint: {
+        type: String,
       },
     },
     data() {
@@ -41,6 +46,28 @@
         return this.isFollowedBy
           ? 'フォロー中'
           : 'フォロー'
+      },
+    },
+    methods: {
+      clickFollow() {
+        if (!this.authorized) {
+          alert('フォロー機能はログイン中のみ使用できます')
+          return
+        }
+
+        this.isFollowedBy
+          ? this.unfollow()
+          : this.follow()
+      },
+      async follow() {
+        const response = await axios.put(this.endpoint)
+
+        this.isFollowedBy = true
+      },
+      async unfollow() {
+        const response = await axios.delete(this.endpoint)
+
+        this.isFollowedBy = false
       },
     },
   }
